1b49772d849018473add85a3cfcda3c2f8ac8c3d
2zzzyoung <mzooyoungn@gmail.com>
2023-03-10 22:22:40 +0900

    commit

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
+import AddTodo from "../AddTodo/AddTodo";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
     { id: "123", text: "shopping", status: "active" },
     { id: "124", text: "study", status: "active" },
   ]);
-
+  const handleAdd = (todo) => {
+    console.log(todo);
+    setTodos([...todos, todo]);
+  };
   return (
-    <section>
-      <ul>
-        {todos.map((item) => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </ul>
-    </section>
+    <>
+      <section>
+        <ul>
+          {todos.map((item) => (
+            <li key={item.id}>{item.text}</li>
+          ))}
+        </ul>
+      </section>
+      <AddTodo onAdd={handleAdd} />
+    </>
   );
 }
